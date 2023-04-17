32bc2f2df06c642ba1d5c707bd4d6094a76f28ef
Podin Victor <podin_victor@mail.ru>
2023-04-17 16:38:51 +0300

    first commit

---
+++ lesson1/src/main/java/ru/geekbrains/VendingMachine.java
@@ -0,0 +1,25 @@
+package ru.geekbrains;
+
+import java.util.List;
+
+public class VendingMachine {
+
+    private final List<Product> products;
+
+    public VendingMachine(List<Product> products) {
+        this.products = products;
+    }
+
+    public BottleOfWater getBottleOfWater(String name, int volume){
+        for (Product product: products) {
+            if (product instanceof BottleOfWater){
+                BottleOfWater bottle = ((BottleOfWater)product);
+                if (bottle.name.equals(name) && bottle.getVolume() == volume)
+                    return bottle;
+            }
+        }
+        return null;
+    }
+
+
+}
---
+++ lesson1/src/main/java/ru/geekbrains/sample1/Sample.java
@@ -0,0 +1,14 @@
+package ru.geekbrains.sample1;
+
+import ru.geekbrains.Product;
+
+public class Sample {
+
+    public static void main(String[] args) {
+
+        Product product = new Product();
+
+
+    }
+
+}
